19374b862734d5e5f023e8568682f549f7864454
victor-914 <okaforv914@gmail.com>
2026-07-16 18:21:57 +0100

    feat(next.config.ts): add standalone output configuration to enable Docker deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   compiler: {
     styledComponents: true
   },
@@ -13,5 +14,7 @@ const nextConfig: NextConfig = {
     ]
   }
 };
+
+
 
 export default nextConfig;
